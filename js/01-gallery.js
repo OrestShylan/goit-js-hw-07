837d93fe9a5418825850571ca3d3d8c9df5121f0
OrestShylan <orestshylan1997@gmail.com>
2023-01-20 16:29:36 +0200

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+onst gallery  = document.querySelector('.gallery');
+
+const galleryMarkup = creatsImgGellery(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function creatsImgGellery(galleryItems){
+    return galleryItems.map(({preview, original, description}) => {
+        return  `
+        <div class="gallery__item"> <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a> 
+        </div>
+    `;
+}).join( ' ');
+    }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,33 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const gallery  = document.querySelector('.gallery');
+
+const galleryMarkup = creatsImgGellery(galleryItems);
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function creatsImgGellery(galleryItems){
+    return galleryItems.map(({preview, original, description}) => {
+        return  `
+        <a class="gallery__item" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+    `;
+}).join( ' ');
+    }
+
+
+    var lightbox = new SimpleLightbox('.gallery a', { 
+        captions: true,
+        captionDelay: 250,
+        captionsData: 'alt',
+     });
+
